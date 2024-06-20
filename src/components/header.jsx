21705c987c4bbd03e4bab8d5dbc2c92bc9696671
Alok <50006236@mahindra.com>
2024-06-20 23:28:37 +0530

    removed title

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,11 +8,12 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
+                {/* <h1>
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
-                </h1>
+                </h1> */}
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <p>{props.data ? props.data.title : "Loading"}</p>
                 {/* <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
